85a20d5105b516e94561d0524aaea4efd458391b
Julian Ng <julesnuggy@gmail.com>
2024-01-24 13:03:25 +0000

    Delete pre-made components

--- src/components/Input.jsx
+++
@@ -1,5 +0,0 @@
-export default function Input({ type, value, name, placeholder, handleChange }) {
-    return (
-        <input type={type} placeholder={placeholder} name={name} value={value} onChange={handleChange} />
-    )
-}
