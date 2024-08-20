469be7ab6552104d5969dfb01ee01d8366796505
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-20 18:18:33 +0100

    Completed user register function

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,28 @@ import Input from './components/Input';
 
 export default function App() {
   const [user, setUser] = useState({ username: '', password: '' });
-  const [registerResponse, setRegisterResponse] = useState('');
+  const [registerResponse, setRegisterResponse] = useState("");
   const [loginResponse, setLoginResponse] = useState('');
 
   const register = async (e) => {
     e.preventDefault();
     // Write your register code here
+    console.log("user", user);
 
+   fetch("http://localhost:4000/register", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify({
+       username: user.username,
+       password: user.password,
+     }),
+   })
+     .then((res) => res.json())
+     .then((data) => {
+       setRegisterResponse(data.user);
+     });
   };
 
   const login = async (e) => {
@@ -59,7 +74,7 @@ export default function App() {
         ]}
       />
 
-      {registerResponse && <p>{registerResponse}</p>}
+      {registerResponse && <p>Welcome {registerResponse.username}!</p>}
 
       <h1>Login</h1>
 
